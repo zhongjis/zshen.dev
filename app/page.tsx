ed4e155642b04ecfa6baf52158f5145ad1754783
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-14 12:22:50 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          A developer living in Utah. More to come. 🚀
+          A developer who loves art in any form. Currently living in Utah. More to come. 🚀
         </h2>
       </div>
     </div>
