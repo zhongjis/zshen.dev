fe60d8a80b2a7ddcd09ec6f541c89b1325990115
zshen <zhongjie.x.shen@gmail.com>
2026-03-01 23:34:54 -0800

    i moved to south bay now

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,8 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          A developer who loves art in any form. Currently living in Utah. More to come. 🚀
+          A developer who loves art in any form. Currently living in south bay
+          area. More to come. 🚀
         </h2>
       </div>
     </div>
